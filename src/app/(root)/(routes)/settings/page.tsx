1637a8bba2c303f048c44875cebeafd78fd5c58b
Ansh2004P <anshkpatel15@gmail.com>
2025-07-30 18:51:37 +0530

    settings PlaceHolderPage

---
+++ src/app/(root)/(routes)/settings/page.tsx
@@ -0,0 +1,9 @@
+const SettingsPage = async () => {
+    return (
+        <div className="w-full flex justify-center items-center">
+            <h1>Settings</h1>
+        </div>
+    );
+}
+
+export default SettingsPage;
